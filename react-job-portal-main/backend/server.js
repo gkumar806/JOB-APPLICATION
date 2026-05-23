900d2193042fc37a5656165aaaab6e05af506ba8
Gaurav <gk2918093@gmail.com>
2026-05-23 09:28:18 +0530

    first commit

---
+++ react-job-portal-main/backend/server.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import cloudinary from "cloudinary";
+import fileUpload from "express-fileupload";
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}`);
+});
